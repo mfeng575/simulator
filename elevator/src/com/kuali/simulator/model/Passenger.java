4e89e48642cb29b693b4fb952327c55b2a68d811
Mike Feng <michael_feng@hotmail.com>
2017-05-24 22:56:18 -0500

    there should be passenger object
    easier to model the situation

---
+++ elevator/src/com/kuali/simulator/model/Passenger.java
@@ -0,0 +1,18 @@
+package com.kuali.simulator.model;
+
+public class Passenger {
+	private int initialFloor;
+	private int targetFloor;
+	public int getInitialFloor() {
+		return initialFloor;
+	}
+	public void setInitialFloor(int initialFloor) {
+		this.initialFloor = initialFloor;
+	}
+	public int getTargetFloor() {
+		return targetFloor;
+	}
+	public void setTargetFloor(int targetFloor) {
+		this.targetFloor = targetFloor;
+	}
+}
