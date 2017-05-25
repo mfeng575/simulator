873afef3d6cfe5cbd73b1e68c2e7d8b5f15c0ec4
Mike Feng <michael_feng@hotmail.com>
2017-05-24 22:16:27 -0500

    put more stuff there
    the elevator needs to run and figure out direction and stops

--- elevator/src/com/kuali/simulator/controller/ElevatorController.java
+++ elevator/src/com/kuali/simulator/controller/ElevatorController.java
@@ -15,17 +15,28 @@ public class ElevatorController {
 		
 		for(int i = 0; i < totalElevator; i++)
 		{
-			elevators.add(new Elevator("Elevator " + i, 1));
+			elevators.add(new Elevator("Elevator " + i, 1, this));
 		}		
 	}
 
+	public void addRequest(int intialFloor, int targetFloor)
+	{
+		// simulate passenger request
+		
+		// TODO find out which elevator is closest and book it
+		
+	}
+	
 	public static void main(String[] args)
 	{
 		try
 		{
 			// start the test
-			ElevatorController controller = new ElevatorController(10, 3);			
+			ElevatorController controller = new ElevatorController(10, 3);	
+			
+			Thread.sleep(30000);
 
+			controller.addRequest(1, 4);
 		}
 		catch (Exception e)
 		{
