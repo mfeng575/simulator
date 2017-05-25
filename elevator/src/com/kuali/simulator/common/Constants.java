7f86fcd97ef3ec9fbd1d42ff8cfab44fb8d97392
Mike Feng <michael_feng@hotmail.com>
2017-05-24 22:35:31 -0500

    focus on the elevator
    set up the direction and state so that elevator is realistic

--- elevator/src/com/kuali/simulator/common/Constants.java
+++ elevator/src/com/kuali/simulator/common/Constants.java
@@ -1,8 +1,9 @@
 package com.kuali.simulator.common;
 
 public interface Constants {
-	public int timeMovingBetweenFloor = 5; // 5 seconds
-	public int timeOpenCloseDoor = 20; // 20 seconds
+	public int timeMovingBetweenFloor = 5000; // 5 seconds
+	public int timeOpenCloseDoor = 20000; // 20 seconds
 	
 	public enum DIRECTION {UP, DOWN, NONE};
+	public enum ELEVATOR_STATE {IDLE, TRANSIT, LOADING};
 }
